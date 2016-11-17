e60cc9e770772a4fd8ea8eeeb884f2ce35308c28
Slawecky <scygert@sigma.ug.edu.pl>
2016-11-17 19:03:54 +0100

    Skopiowane zadania

---
+++ Elementarz2/zad2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  /* deklaracje zmiennych */
+  printf("Podaj liczbe calkowita: ");
+  /* wczytaj tę liczbę do zmiennej n */
+  /* sprawdź czy n jest liczbą pierwszą */
+  printf("Podana liczba '%d' jest %s\n",
+    n, jest_pierwsza ? "pierwsza" : "złożona");
+  return 0;
+}
+Rozmawiają dwaj profesorowie matematyki:
+– Dasz mi swój nr telefonu?
+– No pewnie. Trzecia cyfra jest trzykrotnością pierwszej. Czwarta i szósta są takie same. Druga jest większa o jeden od piątej. Suma sześciu cyfr to 23, a iloczyn 2160.
+– W porządku, zapisałem – 256 343.
+– Zgadza się. Nie zapomnisz?
+– Skądże. To kwadrat 16 i sześcian 7.
+Dilbert & Math
---
+++ Elementarz2/zad4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   /** deklaracje zmiennych lokalnych */
+   nb = 0;  /* liczba znaków odstępu */
+   nt = 0;  /* liczba znaków tabulacji */
+   nl = 0;  /* liczba znaków nowego wiersza */
+   while ((c=getchar()) != EOF) {
+      /** zliczaj białe znaki */
+   }
+   printf("liczba znaków odstępu = %d, \
+      tabulacji = %d, nowego wiersza = %d\n", ... );
+   return 0;
+}
