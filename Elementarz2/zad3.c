503f7ad1200d1ddcbfce3e697778321803e9576b
Slawecky <scygert@sigma.ug.edu.pl>
2016-11-19 12:43:10 +0100

    Do domu

--- Elementarz2/zad3.c
+++ Elementarz2/zad3.c
@@ -1,34 +1,56 @@
 /** Pobrane od Dawida M. */
 #include <stdio.h>
-double przyblizenie_pi(int o) { // comment 1/3 mozna zrobic w int main
+
+void print_table_header() {
+  printf("|liczba składnikow|przyblizenie pi|\n");
+  printf("|---|---|\n");
+}
+
+void przyblizenie_pi_do() {
   int n=0,c=0;
   double pi=0;
-  while(n<o) {
-    if (c==0 && pi>=3.14 && pi<=3.15) {
+  printf("|Przyblizenie osiagnelo|przy elemencie nr|\n");
+  printf("|---|---|\n");
+  while(c!=3) {
+    if (c == 0 && pi >= 3.14 && pi <= 3.15) {
       c=1;
-      printf("Przyblizenie osiagnelo 3.14 przy elemencie nr %d\n", n);
-    }
-    else if (c==1 && pi>=3.141 && pi<=3.142) {
+      printf("|3.14|%d|\n", n);
+    } else if (c == 1 && pi >= 3.141 && pi <= 3.142) {
       c=2;
-      printf("Przyblizenie osiagnelo 3.141 przy elemencie nr %d\n", n);
-    }
-    else if (c==2 && pi>=3.1415 && pi<=3.1416) {
+      printf("|3.141|%d|\n", n);
+    } else if (c == 2 && pi >= 3.1415 && pi <= 3.1416) {
       c=3;
-      printf("Przyblizenie osiagnelo 3.1415 przy elemencie nr %d\n", n);
+      printf("|3.1415|%d|\n", n);
     }
-    if (n%2==0) {
-      pi=pi + 4.0/( n*2.0+1.0 ); // comment 2/3 ale trzeba uzywac 4.0 itd.
-    }
-    else {
-      pi=pi - 4.0/( n*2.0+1.0 ); // comment 3/3 inaczej liczy jak int
+    if (n % 2 == 0) {
+      pi += 4.0/( n * 2.0 + 1.0 );
+    } else {
+      pi -= 4.0/( n * 2.0 + 1.0 );
     }
     n++;
   }
-  return pi;
 }
+
+void przyblizenie_pi(int o) { // comment 1/3 mozna zrobic w int main
+  int n=0;
+  double pi=0;
+  while(n < o) {
+    if (n % 2 == 0) {
+      pi += 4.0/( n * 2.0 + 1.0 ); // comment 2/3 ale trzeba uzywac 4.0 itd.
+    } else {
+      pi -= 4.0/( n * 2.0 + 1.0 ); // comment 3/3 inaczej liczy jak int
+    }
+    n++;
+  }
+  printf("|%d|%lf|\n",o , pi);
+}
+
 int main() {
-  printf("10 elementow %lf\n", przyblizenie_pi(10));
-  printf("100 elementow %lf\n", przyblizenie_pi(100));
-  printf("1000 elementow %lf\n", przyblizenie_pi(1000));
-  printf("1 000 000 elementow %lf\n", przyblizenie_pi(1000000));
+  print_table_header();
+  przyblizenie_pi(10);
+  przyblizenie_pi(100);
+  przyblizenie_pi(1000);
+  przyblizenie_pi(1000000);
+  printf("\n");
+  przyblizenie_pi_do();
 }
---
+++ Elementarz2/zad3a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// π ≈ 3,14159 26535 89793 23846
+
+double leibniz(int n) {
+  double pi = 4;
+  double dziel = 3;
+  int znak = 1;
+
+for(int i = 0; i < n; i++, dziel += 2) {
+  if (znak == 1) {
+    znak = -1;
+}  else  {
+    znak = 1;
+  }
+  pi += znak * (4 / dziel);
+}
+  return pi
+
+int int main() {
+  int liczba_składników[] = (0, 100, 1000,1000000, 1000000000)
+  for (int i= 0; i<5; i++)
+  printf("%f\n", pi);
+return 0;
+}
