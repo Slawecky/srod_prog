43cdb2d480e862680ddc05f7eceb03194b7cedbc
Slawecky <scygert@sigma.ug.edu.pl>
2016-11-19 11:21:11 +0100

    Poprawki

---
+++ Elementarz2/zad3b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  double pi = 4;
+  int x;
+  double dziel = 3;
+  int znak = 1;
+
+printf("Podaj liczbę naturalną : ");
+scanf("%d\n", &x );
+
+for(int i = 0; i < x; i++, dziel += 2) {
+  if (znak==1) {
+    znak = -1;
+}  else  {
+    znak = 1;
+  }
+  pi += znak * (4 / dziel);
+}
+printf("%f\n", pi);
+return 0;
+}
--- Petle/zad2.c
+++ Petle/zad2.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
+
+int even(int k) {
+  return k % 2 == 0;
+}
+
+void print_line(int p, int r, char a, char b) {
+    if (even(r)) {
+      char c = a;
+      a = b;
+      b = c;
+    }
+    for (int y = 0; y < p; y++)
+      printf("%c%c", a, b);
+    printf("\n");
+}
+
 int main() {
+  int a, b;
+  printf("Podaj wymiar x*y\\t znaki\n");
+  scanf("%d %d\t", &a, &b);
+  char c, d;
+  scanf("%c%c", &c, &d);
+  for (int x = 0; x < b; x++)
+    print_line(a, x, c, d);
 }
