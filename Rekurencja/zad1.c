24f14a32ed1f5da8e0f227af91fbec25b770ce2a
Slawecky <scygert@sigma.ug.edu.pl>
2016-12-17 10:14:08 +0100

    Zad 1 i 2

--- Rekurencja/zad1.c
+++ Rekurencja/zad1.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+
+int nk(int podstawa, int  wykladnik);
+
 int main() {
-  return 0;
+  printf(" 5^4 = %d\n ", nk(5, 4));
+  printf(" 5^0 = %d\n ", nk(5, 0));
 }
+
+    int nk(int podstawa, int wykladnik) {
+      if (wykladnik == 0 )
+      return 1;
+      else
+      return podstawa * nk(podstawa, wykladnik - 1);
+    }
