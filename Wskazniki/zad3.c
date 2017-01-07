7b3e3e8d1a3f0f496719f1d9c3d86f1814654c0c
Slawecky <scygert@sigma.ug.edu.pl>
2017-01-07 12:00:16 +0100

    End_of_Lab

--- Wskazniki/zad3.c
+++ Wskazniki/zad3.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-int main() {
-  return 0;
+
+
+  void podnies_do_kwadratu(int *n) {
+    int k = *n;
+    *n = k * k;
+
 }
+
+void wczytaj_liczbe(int *n) {
+  printf("Wpisz liczbę naturalną: ");
+  scanf("%d",n);
+
+}
+
+int main() {
+   int liczba;
+   wczytaj_liczbe(&liczba);
+   podnies_do_kwadratu(&liczba);
+   printf("Kwadrat wczytanej liczby to %d\n", liczba);
+   return 0;
+ }
