350ec9e389fddd9a721311af002884715acf2562
Slawecky <scygert@sigma.ug.edu.pl>
2017-01-14 09:51:20 +0100

    Lab 2017-01-14

--- Wskazniki/zad4.c
+++ Wskazniki/zad4.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+
+int diff_max_min(int length, int array[]) {
+  int min, max;
+  min = max = array[0];
+  for(int i = 1; i < length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    } else if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return max-min;
+}
+
 int main() {
-  return 0;
+  //tester
+  int a[10] = {1, 8, -7, 2, 3, 10, 21, 7, 7, 1};
+  printf("%d\n", diff_max_min(10, a));
 }
--- Wskazniki/zad5.c
+++ Wskazniki/zad5.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
 int main() {
-  return 0;
+  int tab[512];
+  int *wsk = tab;
+  for(int i = 0; i < 512; i++, wsk++) {
+    *wsk=128;
+  }
+  for(int i = 0; i < 512; i += 66) {
+    printf("%d\n", tab[i]);
+  }
 }
